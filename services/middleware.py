92927fa50f14f0e663c94b9bfe7a5b18c81102c2
pego149 <pego149@gmail.com>
2020-05-14 12:46:41 +0200

    Updated csrf for admin - 1.2.3

--- services/middleware.py
+++ services/middleware.py
@@ -1,10 +1,14 @@
-class CustomHeaders:
+from django.urls import reverse
 
+
+class CustomHeaders:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
+        if request.path.startswith(reverse('admin:index')):
+            return response
         response['Content-type'] = "text/xml"
         response['Connection'] = "close"
         response['Expires'] = "-1"
